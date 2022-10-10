e1d164c9ad9d1a30c989a9e16983448fe29194b8
Mariia Moroz <98117895+zoroMari@users.noreply.github.com>
2022-10-10 23:57:29 +0300

    fixedBugs (#1)
    added interfaces for converter service, cleaned components

--- src/app/converter.service.ts
+++ src/app/converter.service.ts
@@ -1,11 +1,24 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 
-@Injectable({providedIn: 'root'})
+interface IApiResponse {
+  result: number;
+}
+
+interface IArguments {
+  amount_from: number;
+  cur_from: string;
+  cur_to: string;
+}
+
+@Injectable({ providedIn: 'root' })
 export class ConverterService {
   constructor(private _http: HttpClient) {}
 
-  public fetchExchangeRate(amount_from: number, cur_from: string, cur_to: string) {
-    return this._http.get(`https://api.exchangerate.host/convert?from=${cur_from}&to=${cur_to}&amount=${amount_from}`)
+  public fetchExchangeRate(args: IArguments): Observable<IApiResponse> {
+    const { amount_from, cur_from, cur_to } = args;
+    return this._http.get<IApiResponse>(`https://api.exchangerate.host/convert?from=${cur_from}&to=${cur_to}&amount=${amount_from}`)
   }
 }
+
--- src/app/converter/converter.component.ts
+++ src/app/converter/converter.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ConverterService } from '../converter.service';
+import { currencies_available } from '../currencies_available';
 
 @Component({
   selector: 'app-converter',
@@ -8,15 +9,14 @@ import { ConverterService } from '../converter.service';
   styleUrls: ['./converter.component.sass']
 })
 export class ConverterComponent implements OnInit {
-  public currencies = ['USD', 'EUR', 'UAH'];
+  private _cur_avail = currencies_available;
+  public currencies = [this._cur_avail.usd, this._cur_avail.eur, this._cur_avail.uah];
   public formCurrency_1!: FormGroup;
   public formCurrency_2!: FormGroup;
   public error: boolean = false;
-  private resObj;
 
   private _cur_1: string;
   private _amount_1: number;
-
   private _cur_2: string;
   private _amount_2: number;
 
@@ -27,12 +27,12 @@ export class ConverterComponent implements OnInit {
 
     this.formCurrency_1 = new FormGroup({
       amount: new FormControl('0', Validators.required),
-      currency: new FormControl('USD', Validators.required)
+      currency: new FormControl(this._cur_avail.usd, Validators.required)
     })
 
     this.formCurrency_2 = new FormGroup({
       amount: new FormControl('0', Validators.required),
-      currency: new FormControl('UAH', Validators.required)
+      currency: new FormControl(this._cur_avail.uah, Validators.required)
     })
 
     this._cur_1 = this.formCurrency_1.get('currency')?.value;
@@ -45,33 +45,32 @@ export class ConverterComponent implements OnInit {
     this._cur_1 = this.formCurrency_1.get('currency')?.value;
     this._amount_1 = +this.formCurrency_1.get('amount')?.value;
     this._cur_2 = this.formCurrency_2.get('currency')?.value;
-    this._amount_2 = +this.formCurrency_2.get('amount')?.value;
 
     this._converCurrency(this.formCurrency_2, this._cur_1, this._amount_1, this._cur_2)
   }
 
   public handleChange_2() {
     this._cur_1 = this.formCurrency_1.get('currency')?.value;
-    this._amount_1 = +this.formCurrency_1.get('amount')?.value;
     this._cur_2 = this.formCurrency_2.get('currency')?.value;
     this._amount_2 = +this.formCurrency_2.get('amount')?.value;
 
     this._converCurrency(this.formCurrency_1, this._cur_2, this._amount_2, this._cur_1)
   }
 
-  private _converCurrency(to_form: FormGroup, from_cur: string, from_amount:number, to_cur: string) {
+  private _converCurrency(to_form: FormGroup, from_cur: string, from_amount: number, to_cur: string) {
     if(from_amount === 0) return;
 
-    this.converterService.fetchExchangeRate(from_amount, from_cur, to_cur)
+    this.converterService.fetchExchangeRate(
+      {
+        amount_from: from_amount,
+        cur_from: from_cur,
+        cur_to: to_cur,
+      }
+    )
       .subscribe(
-        result => {
-
+        amount => {
           this.error = false;
-
-          this.resObj = result;
-
-          const newAmount: number = (this.resObj.result).toFixed(3);
-
+          const newAmount: number = +(amount.result).toFixed(3);
           to_form.patchValue({ 'amount': newAmount });
         },
         error => {
